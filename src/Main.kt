8221bbc1df395398ad1a3d6b93e92dda1090aa11
Marthamwongeli <marthamutuaa@gmail.com>
2024-02-14 08:03:51 +0300

    kotlin 1

---
+++ src/Main.kt
@@ -0,0 +1,18 @@
+fun main() {
+    println("Hello World!")
+    var name: String = "Jane Nafula"
+    println(name)
+    var age: Byte = 25
+    println(age)
+    var weight = 55.4F
+    println(weight)
+    var kenyanCitizenship : Boolean = true
+    println(kenyanCitizenship)
+    var population : Long = 2000000000000000000
+    println(population)
+    var letter : Char = 'j'
+    println(letter)
+    val height = 6.5
+    println(height)
+
+}
